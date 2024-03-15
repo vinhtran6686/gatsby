9e6c17920dc672c9cb42e09e7af9c425b078c806
vinhtran6686 <vinhtran.6686@gmail.com>
2024-03-15 21:23:54 +0700

    Gatsby: 37 - Add order total and order ahead button to OrderPage

--- gatsby/src/pages/orders.js
+++ gatsby/src/pages/orders.js
@@ -9,6 +9,7 @@ import OrderStyles from '../styles/OrderStyles';
 import MenuItemStyles from '../styles/MenuItemStyles';
 import usePizza from '../utils/usePizza';
 import PizzaOrder from '../components/PizzaOrder';
+import calculateOrderTotal from '../utils/calculateOrderTotal';
 
 export default function OrderPage({ data }) {
   const { values, updateValue } = useForm({
@@ -77,6 +78,12 @@ export default function OrderPage({ data }) {
             removeFromOrder={removeFromOrder}
             pizzas={pizzas}
           />
+        </fieldset>
+        <fieldset>
+          <h3>
+            Your Total is {formatMoney(calculateOrderTotal(order, pizzas))}
+          </h3>
+          <button type="submit">Order Ahead</button>
         </fieldset>
       </OrderStyles>
     </>
