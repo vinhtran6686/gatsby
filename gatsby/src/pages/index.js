ca7488c6dcbc173af48d48d42522e9a5276c39b0
vinhtran6686 <vinhtran.6686@gmail.com>
2024-03-17 04:58:23 +0700

    Gatsby: 46- Add GraphQL endpoint and update home page layout

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,7 +1,32 @@
 import React from 'react';
-import Nav from '../components/Nav';
-import Layout from '../components/Layout';
+import useLatestData from '../utils/useLatestData';
 
-const Home = () => <>HOme</>;
+function CurrentlySlicing() {
+  return (
+    <div>
+      <p>CurrentlySlicing</p>
+    </div>
+  );
+}
+function HotSlices() {
+  return (
+    <div>
+      <p>HotSlices</p>
+    </div>
+  );
+}
 
-export default Home;
+export default function HomePage() {
+  const { slicemasters, hotSlices } = useLatestData();
+
+  return (
+    <div className="center">
+      <h1>The Best Pizza Downtown!</h1>
+      <p>Open 11am to 11pm Every Single Day</p>
+      <div>
+        <CurrentlySlicing slicemasters={slicemasters} />
+        <HotSlices hotSlices={hotSlices} />
+      </div>
+    </div>
+  );
+}
